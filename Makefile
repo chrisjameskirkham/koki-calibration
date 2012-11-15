a1b98382366115a17a97da9bfa5ebfbb8638b79f
Chris Kirkham <chrisjameskirkham@gmail.com>
2012-11-15 21:43:54 +0000

    Add a utility to take a photo at a given resolution

--- Makefile
+++ Makefile
@@ -2,10 +2,13 @@ THINGS := glib-2.0 opencv
 CFLAGS += `pkg-config --cflags $(THINGS)`
 LDFLAGS += `pkg-config --libs $(THINGS)` -lkoki
 
-all: dist_at_focal_length
+all: dist_at_focal_length take_photo
 
 dist_at_focal_length: dist_at_focal_length.c
 	$(CC) $(CFLAGS) $< -o $@ $(LDFLAGS)
 
+take_photo: take_photo.c
+	$(CC) $(CFLAGS) $< -o $@ $(LDFLAGS)
+
 clean:
-	rm -rf dist_at_focal_length
+	rm -rf dist_at_focal_length take_photo
---
+++ take_photo.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <cv.h>
+#include <highgui.h>
+#include <stdlib.h>
+#include <linux/videodev2.h>
+#include "koki.h"
+
+int main(int argc, const char* argv[])
+{
+
+	if (argc != 5){
+		printf("USAGE: %s <device> <filename> <width> <height>\n", argv[0]);
+		return 1;
+	}
+
+	const char *device = argv[1];
+	const char *filename = argv[2];
+	float width = atof(argv[3]);
+	float height = atof(argv[4]);
+
+	int fd = koki_v4l_open_cam(device);
+	struct v4l2_format fmt = koki_v4l_create_YUYV_format(width, height);
+	koki_v4l_set_format(fd, fmt);
+
+	int num_buffers = 1;
+	koki_buffer_t *buffers;
+	buffers = koki_v4l_prepare_buffers(fd, &num_buffers);
+
+	koki_v4l_start_stream(fd);
+
+	uint8_t *yuyv = koki_v4l_get_frame_array(fd, buffers);
+	IplImage *frame = koki_v4l_YUYV_frame_to_grayscale_image(yuyv, width, height);
+	cvSaveImage(filename, frame, 0);
+
+	cvReleaseImage(&frame);
+	koki_v4l_stop_stream(fd);
+
+	return 0;
+}
